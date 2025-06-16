963bd071922253e149f89e93d3dd0a4056192f99
bonji <bonji@mikuro.jp>
2025-06-17 04:53:09 +0900

    fix:

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,24 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { mdsvex } from 'mdsvex';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+/** @type {import('mdsvex').MdsvexOptions} */
+const mdsvexOptions = {
+	extensions: ['.svx'],
+	layout: {
+		// 絶対パスで指定（推奨）
+		_: join(__dirname, './src/mdsvex.svelte')
+	}
+};
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	extensions: ['.svelte', '.svx'],
-	preprocess: [vitePreprocess(), mdsvex({
-		extensions: ['.svx'],
-		layout: { _: './src/mdsvex.svelte' }
-	})],
+	preprocess: [vitePreprocess(), mdsvex(mdsvexOptions)],
 	kit: {
 		adapter: adapter()
 	}
